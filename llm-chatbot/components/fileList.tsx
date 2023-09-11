fa0c62f43f1d20872236107977aa55e05e1d738a
Rahul Kumar <rahul.k@smarter.codes>
2023-09-11 20:20:45 +0530

    added progressBar

--- llm-chatbot/components/fileList.tsx
+++ llm-chatbot/components/fileList.tsx
@@ -1,13 +1,24 @@
 import styles from '@/styles/Home.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-export default function FileList({ filename
-}: { filename: string }) {
+export default function FileList({ filename, removefilefromfileList, index
+}: { filename: string, removefilefromfileList: (index: number) => void, index: number }) {
 
 
-    const removeSelectedField = () => {
-        console.log("remove")
-    }
+    const [progress, setProgress] = useState(0);
+
+    useEffect(() => {
+        let interval: any;
+        if (progress < 100) {
+            interval = setInterval(() => {
+                setProgress(prevProgress => prevProgress + 1);
+            }, 100); // 1200 milliseconds (2 minutes divided by 100 steps)
+        }
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [progress]);
     return (
         <ul className="bg-white rounded-lg shadow divide-y divide-gray-200 max-w-sm mt-2">
             <li className="px-6 py-4">
@@ -18,9 +29,17 @@ export default function FileList({ filename
                     <div className='flex flex-col items-center'>
 
                         <span className="font-semibold text-lg">{filename}</span>
-                        <p className="text-gray-700">AI Trained</p>
+                        {
+                            progress !== 100 ?
+                                <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                                    <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: `${progress}%` }}> {progress}%</div>
+                                </div> :
+                                null
+
+                        }
+                        <p className="text-gray-700">{progress === 100 ? "AI Trained" : "Taining AI ..."}</p>
                     </div>
-                    <span className="text-gray-500 text-xs"><button className={styles.crossiconButton} onClick={removeSelectedField}  >
+                    <span className="text-gray-500 text-xs"><button className={styles.crossiconButton} onClick={() => removefilefromfileList(index)}  >
                         <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className={styles.crossicon}><path fillRule="evenodd" clipRule="evenodd" d="M6.207 4.793a1 1 0 0 0-1.414 1.414L10.586 12l-5.793 5.793a1 1 0 1 0 1.414 1.414L12 13.414l5.793 5.793a1 1 0 0 0 1.414-1.414L13.414 12l5.793-5.793a1 1 0 0 0-1.414-1.414L12 10.586 6.207 4.793Z"></path></svg>
                     </button></span>
                 </div>
