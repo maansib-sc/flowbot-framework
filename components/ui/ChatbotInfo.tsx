3fbd5f2f7217fbe506a5a93b5089d552e15c3fb9
Rahul Kumar <rahul.k@smarter.codes>
2023-10-27 12:39:00 +0530

    update input accept

--- components/ui/ChatbotInfo.tsx
+++ components/ui/ChatbotInfo.tsx
@@ -162,7 +162,7 @@ const ChatbotInfo = ({
                 <div className='flex mb-6'>
 
                     <label className="w-64 flex justify-between items-center px-2 py-2 text-blue rounded-lg  tracking-wide  border border-blue  cursor-pointer">
-                        <input type='file' accept='.pdf' className="hidden" onChange={handlePDFFileChange} ref={fileInputRef} />
+                        <input type='file' accept='.pdf,.docx' className="hidden" onChange={handlePDFFileChange} ref={fileInputRef} />
                         <span className="mt-2 text-base leading-normal">Upload a Doc</span>
                         <svg className="w-8 h-8 pt-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                             <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
