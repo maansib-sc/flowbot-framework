4f4ee3716fafcf9a6885b2e5747b0a169505ade0
Rahul Kumar <rahul.k@smarter.codes>
2023-10-27 19:24:10 +0530

    update progress

--- components/fileList.tsx
+++ components/fileList.tsx
@@ -19,15 +19,16 @@ export default function FileList({ selectedFileType, filename, index, trained, s
                 const response = await pdfFileProgress(filename)
                 if (response) {
                     setProgress(Math.ceil(Number(response.data.data.deep.replace(/%/g, ""))))
-                    if (response.data.data.shallow === "100.0%") {
-                        setTimeout(() => {
-                            setIs_Shallow_Trained(true)
-                        }, 10000)
-                    }
-
-                    if (response.data.data.deep === "100.0%") {
+                    let deepdataProgress = Math.ceil(Number(response.data.data.deep.replace(/%/g, "")))
+                    let shallowdataProgress = Math.ceil(Number(response.data.data.deep.replace(/%/g, "")))
+                    if (deepdataProgress === 100) {
                         setTimeout(() => {
                             setIs_Trained(true)
+                        }, 10000)
+                    }
+                    if (shallowdataProgress === 100) {
+                        setTimeout(() => {
+                            setIs_Shallow_Trained(true)
                         }, 10000)
                     }
                 }
@@ -52,18 +53,21 @@ export default function FileList({ selectedFileType, filename, index, trained, s
 
     useEffect(() => {
         if (selectedFileType === "PDF") {
-            if (!trained && progress !== 100) {
-                let interval: any;
-                if (progress < 100) {
-                    interval = setInterval(() => {
-                        pdfProgress()
-                    }, 1200); // 1200 milliseconds (2 minutes divided by 100 steps)
-                }
 
-                return () => {
-                    clearInterval(interval);
-                };
-            }
+            if (progress)
+
+                if (!trained && progress !== 100) {
+                    let interval: any;
+                    if (progress < 100) {
+                        interval = setInterval(() => {
+                            pdfProgress()
+                        }, 1200); // 1200 milliseconds (2 minutes divided by 100 steps)
+                    }
+
+                    return () => {
+                        clearInterval(interval);
+                    };
+                }
 
             if (!trained && progress === 100) {
                 setTimeout(() => {
