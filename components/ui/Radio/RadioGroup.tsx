45526e3516821e5b850e9b4b6bf5fe775e63c2ed
Rahul Kumar <rahul.k@smarter.codes>
2023-11-10 17:02:36 +0530

    updated radio Group

--- components/ui/Radio/RadioGroup.tsx
+++ components/ui/Radio/RadioGroup.tsx
@@ -3,32 +3,34 @@ import styles from '@/configuration/CSS/Index.module.css';
 
 const RadioGroup = ({
   options,
+  value,
   onChange,
 }: {
   options: { label: string; value: string }[];
+  value: string;
   onChange: (value: string) => void;
 }) => {
 
-  const [selectedValue, setSelectedValue] = useState(0)
+  const [selectedValue, setSelectedValue] = useState<string | null>(value || null)
 
-  const changeSelectedValue = (index: number) => {
-    setSelectedValue(index)
-    onChange(options[index].value)
+  const changeSelectedValue = (value: string) => {
+    setSelectedValue(value)
+    onChange(value)
   }
   return (
     <div className={styles.radioGroup}> {/* Apply a class from the imported CSS module */}
       {options.map((option, index) => (
         <label
           key={option.value}
-          className={`${styles.radioLabel} ${selectedValue === index ? styles.selected : ''}`}
+          className={`${styles.radioLabel} ${selectedValue === option.value ? styles.selected : ''}`}
         >
           {option.label}
           <input
             type="radio"
             value={option.value}
-            checked={selectedValue === index}
+            checked={selectedValue === option.value}
             onChange={() => {
-              changeSelectedValue(index)
+              changeSelectedValue(option.value)
             }}
             className={styles.radioInput}
           />
