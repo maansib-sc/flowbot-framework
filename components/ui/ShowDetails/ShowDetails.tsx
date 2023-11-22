896247a4adae8248b1a1fdaa90e297ef6e32d0c7
Rahul Kumar <rahul.k@smarter.codes>
2023-11-22 10:37:55 +0530

    updated showDetail component

--- components/ui/ShowDetails/ShowDetails.tsx
+++ components/ui/ShowDetails/ShowDetails.tsx
@@ -2,28 +2,28 @@ import React, { useState } from 'react';
 import styles from '@/styles/Home.module.css';
 import Button from '../Buttons/Button';
 
-const ShowDetails = ({ onSave }: { onSave: () => void }) => {
+const ShowDetails = ({ options, onSave }: { options: any, onSave: () => void }) => {
   const [showButton, setShowButton] = useState<Boolean>(true)
   return (
     <div className={styles.detailsContainer}>
-      <h3>PERRONE CONSTRUCTION INCORPORATED</h3>
+      <h3>{options.name}</h3>
       <div className={styles.detailsFlex}>
         <div className={styles.detailsShow}>
           <h6>License Number</h6>
-          <span>CBC017177</span>
+          <span>{options.license_id}</span>
         </div>
         <div className={styles.detailsShow}>
           <h6>Date</h6>
-          <span>08/31/2024</span>
+          <span>{options.license_date}</span>
         </div>
       </div>
       <div className={styles.detailsShow}>
         <h6>License Location Address</h6>
-        <span>7045 S TAMIAMI TRAIL SARASOTA, FL 34231</span>
+        <span>{options.address}</span>
       </div>
       <div className={styles.detailsShow}>
         <h6>Main Address</h6>
-        <span>7045 S TAMIAMI TRAIL SARASOTA, FL 34231</span>
+        <span>{options.address}</span>
       </div>
       {showButton && <div>
         <Button onClick={() => { onSave(); setShowButton(false) }}>Confirm</Button>
