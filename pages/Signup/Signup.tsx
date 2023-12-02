43d8466cd185f8403f466a59302cef457c7e1b57
Rahul Kumar <rahul.k@smarter.codes>
2023-12-02 22:22:05 +0530

    icon fixed

--- pages/Signup/Signup.tsx
+++ pages/Signup/Signup.tsx
@@ -598,6 +598,7 @@ const Signup = () => {
                                       'cardRadio' ? (
                                       <CardRadioGroup
                                         value={message?.step?.default}
+                                        disabled={index !== messages.length - 1 ? true : false}
                                         onChange={(value) => {
                                           if (index === messages.length - 1) {
                                             handleSubmit(value);
@@ -811,19 +812,21 @@ const Signup = () => {
                       type="submit"
                       disabled={!query || loading}
                       className={homestyles?.generatebutton}
+                      style={{background: JSModule?.sendIcon && !loading ? JSModule?.themeColor : ''}}
                     >
                       {loading ? (
                         <div className={homestyles?.loadingwheel}>
                           <LoadingDots color="#000" />
                         </div>
                       ) : (
-                        <svg
-                          viewBox="0 0 20 20"
-                          className={homestyles?.svgicon}
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+                        <div
+                          dangerouslySetInnerHTML={{ __html: JSModule?.sendIcon ?? `
+                          <svg viewBox="0 0 20 20" class="Home_svgicon__PLaWz" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z">
+                            </path>
                         </svg>
+                          ` }}
+                        />
                       )}
                     </button>
                   </form>
